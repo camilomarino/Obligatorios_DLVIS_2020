055fa3031708bd80f59305dee21e42f8de98bb1c
Camilo M <camilo.marino.cabrera@gmail.com>
2020-10-27 18:50:57 -0300

    Se agregan metodos de optimizacion

--- assignment2_jupyter/cs231n/optim.py
+++ assignment2_jupyter/cs231n/optim.py
@@ -108,7 +108,11 @@ def rmsprop(w, dw, config=None):
     ###########################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-    pass
+    config['cache'] = (config['decay_rate'] * config['cache'] + 
+                        (1 - config['decay_rate']) * dw**2
+                        )
+    next_w = w - config['learning_rate'] * dw / (np.sqrt(config['cache']) + 
+                                              config['epsilon'])
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
     ###########################################################################
@@ -152,8 +156,16 @@ def adam(w, dw, config=None):
     # using it in any calculations.                                           #
     ###########################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+    
+    config['t'] += 1
+    
+    config['m'] = config['beta1']*config['m'] + (1-config['beta1']) * dw
+    mt = config['m'] / (1-config['beta1']**config['t'])
 
-    pass
+    config['v'] = config['beta2']*config['v'] + (1-config['beta2']) * (dw**2)
+    vt = config['v'] / (1-config['beta2']**config['t'])
+    
+    next_w = w - config['learning_rate'] * mt / (np.sqrt(vt) + config['epsilon'])
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
     ###########################################################################
